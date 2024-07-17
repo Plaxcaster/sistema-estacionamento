a99e6f6afc36ebb69fc06cefb87dbf229936ad1a
Pedro Plá <pedro.bpla@gmail.com>
2024-07-17 20:39:10 -0300

    Commit código base Spring Initializr, Spring Boot 3.3.1, Java 17

---
+++ src/test/java/horizonleap/sistema_estacionamento/SistemaEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package horizonleap.sistema_estacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
