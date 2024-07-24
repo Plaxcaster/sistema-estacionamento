7171c6b4704defd14432632b8d20d3a14f1e6af1
Harryalerta <harryalerta@gmail.com>
2024-07-24 19:45:34 -0300

    Retirada do código temporário

--- src/main/java/horizonleap/sistema_estacionamento/SistemaEstacionamentoApplication.java
+++ src/main/java/horizonleap/sistema_estacionamento/SistemaEstacionamentoApplication.java
@@ -2,9 +2,8 @@ package horizonleap.sistema_estacionamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class SistemaEstacionamentoApplication {
 
 	public static void main(String[] args) {
