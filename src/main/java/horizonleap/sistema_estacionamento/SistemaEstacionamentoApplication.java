2c7a983d15d3328f7cdb441ec65587353f374cc2
Harryalerta <harryalerta@gmail.com>
2024-07-24 16:50:22 -0300

    Essa marcação foi necessária para fazer funcionar. Talvez tenhamos que retirar depois.

--- src/main/java/horizonleap/sistema_estacionamento/SistemaEstacionamentoApplication.java
+++ src/main/java/horizonleap/sistema_estacionamento/SistemaEstacionamentoApplication.java
@@ -2,8 +2,9 @@ package horizonleap.sistema_estacionamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SistemaEstacionamentoApplication {
 
 	public static void main(String[] args) {
