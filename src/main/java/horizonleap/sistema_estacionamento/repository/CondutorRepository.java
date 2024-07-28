1d5430af6f47e5fa06e9a78259140d9e1eefc902
Harryalerta <harryalerta@gmail.com>
2024-07-28 12:19:07 -0300

    Incluir import

--- src/main/java/horizonleap/sistema_estacionamento/repository/CondutorRepository.java
+++ src/main/java/horizonleap/sistema_estacionamento/repository/CondutorRepository.java
@@ -1,6 +1,7 @@
 package horizonleap.sistema_estacionamento.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import horizonleap.sistema_estacionamento.model.Condutor;
 
